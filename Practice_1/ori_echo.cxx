7459b1fca35bf72122e001b569ea04da6fb4389a
DOZOU <akadama13@hotmail.co.jp>
2017-12-16 14:47:51 +0900

    echoの複数表示に対応させよう:section2

--- Practice_1/ori_echo.cxx
+++ Practice_1/ori_echo.cxx
@@ -3,10 +3,15 @@
 int main(int argv, char **argc)
 {
     std::string val(argc[1]);
+
     if(val=="-v"){
         std::cout << "Version:0.0.1" << std::endl;
-    }else{
-        std::cout << val << std::endl;
+        return 0;
     }
+
+    for(int i=1;i<argv;i++){
+        std::cout << std::string(argc[i]) << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
